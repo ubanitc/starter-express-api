f62c7738d2e20cd5cfbd4b619d9e1fffb4666db4
Ubani Tochukwu <57474291+ubanitc@users.noreply.github.com>
2023-04-17 09:27:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require("express");
+const { Configuration, OpenAIApi } = require("openai");
+const app = express();
+const port = 3000;
+const path = require("path"); 
+const configuration = new Configuration({
+  apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+app.post("/api/sendPrompt", async (req, res) => {
+  const prompt = req.body.prompt;
+
+  try {
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: prompt }],
+    });
+
+    res.json(completion.data.choices[0].message);
+  } catch (error) {
+    res.status(500).send("Error processing the request.");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
